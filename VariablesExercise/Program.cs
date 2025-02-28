cbad3d99e309c6734ec8c29fb4a694362b589a98
Laptop <micahcorbintanner@gmail.com>
2025-02-27 18:44:19 -0700

    Did the project (:

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Ozzy";
+            int numverOfToys = 60;
+            char gender = 'm';
+            bool happy = true;
+            double age = 2.5;
+            decimal netWorth = 0.00m;
+
+            string genderLongHand = (gender == 'm') ? "boy" : "girl";
+            Console.WriteLine($"My dogs name is {name} he has {numverOfToys} toys!");
+            Console.WriteLine($"{name} is a {genderLongHand} dog");
+            Console.WriteLine($"{(gender == 'm' ? "He" : "She")} is {(happy ? "very happy (:" : "a little sad ):")}");
+            Console.WriteLine($"{name} is {age} years old!");
+            Console.WriteLine($"Unlike me {(gender == 'm' ? "He" : "She")} has ${netWorth} saved up.");
+
+            // I hope you like hearing about my dog he is a very good boy (:
         }
     }
 }
